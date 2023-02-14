a4ff1bc6f4a03b9d9b42340566dce3061dcadf63
raghad-emad <raghademaad163@gmail.com>
2023-02-14 22:47:20 +0200

    update readme

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -11,7 +11,7 @@ export default function Topbar() {
     <div className="topbarContainer">
       <div className="topbarLeft">
         <Link to="/" style={{ textDecoration: "none" }}>
-          <span className="logo">Messenger</span>
+          <span className="logo">Cha-chat</span>
         </Link>
       </div>
       <div className="topbarCenter">
